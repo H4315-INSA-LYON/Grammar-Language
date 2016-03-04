15e0d2b2d54189b57be76d2c55d6496abfe3bf3b
scriam <tertiuc@gmail.com>
2016-03-04 10:45:12 +0100

    varState added

---
+++ src/State/VarState.cpp
@@ -0,0 +1,157 @@
+#include "lutin.h"
+#include "VarState.h"
+//4,7,,27,28,29,30
+using namespace lutinCompiler;
+
+//implementation of the State4 class methods
+State4 :: State4(string name) : State(name)
+{
+    
+}
+
+void State4 ::print()
+{
+    cout<<name;
+}
+
+State4 ::~State()
+{
+
+}
+
+bool State4 ::transition(Automat &automat, Symbol *s)
+{
+    switch(*s)
+    {
+
+    }
+    return false;
+}
+
+//implementation of the State7 class methods
+State7 :: State7(string name) : State(name)
+{
+    
+}
+
+void State7 ::print()
+{
+    cout<<name;
+}
+
+State7 ::~State()
+{
+
+}
+
+bool State7 ::transition(Automat &automat, Symbol *s)
+{
+    switch(*s)
+    {
+
+    }
+    return false;
+}
+
+//implementation of the State27 class methods
+State27 :: State27(string name) : State(name)
+{
+    
+}
+
+void State27 ::print()
+{
+    cout<<name;
+}
+
+State27 ::~State()
+{
+
+}
+
+bool State27 ::transition(Automat &automat, Symbol *s)
+{
+    switch(*s)
+    {
+
+    }
+    return false;
+}
+
+
+//implementation of the State28 class methods
+State28 :: State4(string name) : State(name)
+{
+    
+}
+
+void State28 ::print()
+{
+    cout<<name;
+}
+
+State28 ::~State()
+{
+
+}
+
+bool State28 ::transition(Automat &automat, Symbol *s)
+{
+    switch(*s)
+    {
+
+    }
+    return false;
+}
+
+
+//implementation of the State29 class methods
+State29 :: State4(string name) : State(name)
+{
+    
+}
+
+void State29 ::print()
+{
+    cout<<name;
+}
+
+State29 ::~State()
+{
+
+}
+
+bool State29 ::transition(Automat &automat, Symbol *s)
+{
+    switch(*s)
+    {
+
+    }
+    return false;
+}
+
+//implementation of the State30 class methods
+State30 :: State30(string name) : State(name)
+{
+    
+}
+
+void State30 ::print()
+{
+    cout<<name;
+}
+
+State30 ::~State()
+{
+
+}
+
+bool State30 ::transition(Automat &automat, Symbol *s)
+{
+    switch(*s)
+    {
+
+    }
+    return false;
+}
+
