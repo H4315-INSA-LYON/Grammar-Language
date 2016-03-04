a7d9b2b905162a4db04f9f8333d31c642713a0ae
Haouriqui Amjad <ahaouriqui@if501-219-17.insa-lyon.fr>
2016-03-04 10:33:18 +0100

    je veux faire de algo

--- ConstState.h
+++ ConstState.h
@@ -9,29 +9,72 @@ namespace lutinCompiler
         State5 (string name);
         ~State5();
         void print();
-        bool transition (Automate & automate, Symbole * s) = 0;
+        bool transition (Automat & automat, Symbol * s) = 0;
     };
     class State8: public State
     {
         State8 (string name);
         ~State8();
         void print();
-        bool transition (Automate & automate, Symbole * s) = 0;
+        bool transition (Automat & automat, Symbol * s) = 0;
     };
     class State9: public State
     {
         State9 (string name);
         ~State9();
         void print();
-        bool transition (Automate & automate, Symbole * s) = 0;
+        bool transition (Automat & automat, Symbol * s) = 0;
     };
     class State20: public State
     {
         State20 (string name);
         ~State20();
         void print();
-        bool transition (Automate & automate, Symbole * s) = 0;
+        bool transition (Automat & automat, Symbol * s) = 0;
     };
+    class State21: public State
+    {
+        State21 (string name);
+        ~State21();
+        void print();
+        bool transition (Automat & automat, Symbol * s) = 0;
+    };
+    class State22: public State
+    {
+        State22 (string name);
+        ~State22();
+        void print();
+        bool transition (Automat & automat, Symbol * s) = 0;
+    };
+    class State23: public State
+    {
+        State23 (string name);
+        ~State23();
+        void print();
+        bool transition (Automat & automat, Symbol * s) = 0;
+    };
+    class State24: public State
+    {
+        State24 (string name);
+        ~State24();
+        void print();
+        bool transition (Automat & automat, Symbol * s) = 0;
+    };
+    class State25: public State
+    {
+        State25 (string name);
+        ~State25();
+        void print();
+        bool transition (Automat & automat, Symbol * s) = 0;
+    };
+    class State26: public State
+    {
+        State26 (string name);
+        ~State26();
+        void print();
+        bool transition (Automat & automat, Symbol * s) = 0;
+    };
+
 }
 
 #endif // CONSTSTATE_H
